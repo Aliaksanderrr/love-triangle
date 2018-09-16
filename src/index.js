ba2c81af4b2bd2ad0db2f0dd5c084042b7ce2364
Aliaksanderrr <Midas1989@mail.ru>
2018-09-16 15:59:21 +0300

    my second test

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var triang=0, a, b, c;
+  var triangmass=[];
+    for(var i=0; i<preferences.length; i++) {
+      if (triangmass.includes(i) == false){
+          a=preferences[i];
+          b=preferences[a-1];
+          c=preferences[b-1];
+          if (i==c-1 && a!=b && b!=c){
+            triang=triang+1;
+            triangmass=triangmass.concat(a-1, b-1, c-1);
+          };
+      };
+    };
+return triang;
 };
